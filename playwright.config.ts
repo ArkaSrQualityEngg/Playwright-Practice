07455f42420b7ba4a9898432ccda135672f0dc8c
AIArka1 <arkapravabhattacharya312@gmail.com>
2026-02-21 16:29:39 +0530

    Push 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,16 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -36,7 +25,10 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'],
+         viewport: { width: 1500, height: 1080 },
+      },
+      
     },
 
     // {
@@ -70,10 +62,5 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  
 });
---
+++ tests/browserContext.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect, chromium } from '@playwright/test';
+
+test.describe('Context Switching', async()=> {
+
+    test('Switching between pages',async()=> {
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+        const page1 = await context.newPage();
+        await page1.goto('https:\/\/cognizant\.udemy\.com\/?');
+        await page1.waitForLoadState("domcontentloaded");
+        await expect(page1.getByRole('heading', { name: 'Welcome to Udemy Business' })).toBeVisible();
+        await page1.waitForTimeout(3000);
+
+    });
+});
