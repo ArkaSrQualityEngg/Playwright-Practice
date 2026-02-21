ae774c4fd3e289f53c040ef51763e6ab3fbd9341
AIArka1 <arkapravabhattacharya312@gmail.com>
2026-02-21 16:33:48 +0530

    Push 3

--- tests/browserContext.spec.ts
+++ tests/browserContext.spec.ts
@@ -6,7 +6,7 @@ test.describe('Context Switching', async()=> {
         const browser = await chromium.launch();
         const context = await browser.newContext();
         const page1 = await context.newPage();
-        await page1.goto('https:\/\/cognizant\.udemy\.com\/?');
+        // await page1.goto('https:\/\/cognizant\.udemy\.com\/?');
         await page1.waitForLoadState("domcontentloaded");
         await expect(page1.getByRole('heading', { name: 'Welcome to Udemy Business' })).toBeVisible();
         await page1.waitForTimeout(3000);
